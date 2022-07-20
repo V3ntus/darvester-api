05a5d12e530c8dfd31e38772d6cd1b074f116361
Ventus <29584664+V3ntus@users.noreply.github.com>
2022-07-19 21:47:41 -0500

    add: init rust rocket api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{Build, Rocket, Request};
+#[macro_use] extern crate rocket;
+
+#[catch(404)]
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build()
+        .mount("/", routes![index])
 }
